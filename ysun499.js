aea13c5a60bb90db6d80483607adda23eac3646a
lhsunyc <lhsunyc@outlook.com>
2020-09-17 22:46:38 +0800

    Add register/login page
    Enable buy item feature

--- ysun499.js
+++ ysun499.js
@@ -79,6 +79,8 @@ function DisplayProduct() {
             Price.innerHTML = "$" + data[i].Price;
             Type.innerHTML = data[i].Type;
             button.innerHTML = "Buy";
+            button.id += data[i].ItemId;
+            button.setAttribute("onclick", "buyProduct(this.id)");
 
             products.appendChild(image);
             products.appendChild(Title);
@@ -144,12 +146,10 @@ function getContact() {
 
         for (let i = 0; i < data.length; i++) {
             let sentence = data[i].split(":");
-            console.log(sentence);
             let hrefLink = document.createElement("a");
 
             if (sentence[0].includes("ADR")) {
-                address.innerHTML = "Address: " + sentence[1].replace(';;','');
-                console.log('address');
+                address.innerHTML = "Address: " + sentence[1].replace(';;', '');
             } else if (sentence[0].includes("TEL")) {
                 phone.innerHTML = "Phone: ";
                 hrefLink.setAttribute("href", "tel:" + sentence[1]);
@@ -247,6 +247,134 @@ function postComment() {
         document.getElementById("comment").value = "";
     }
 }
+
+function registerUser() {
+    let username, password, repeat, jreg;
+    username = document.getElementById("username").value;
+    password = document.getElementById("reg-password").value;
+    repeat = document.getElementById("repeat-password").value;
+
+    if (password != repeat) {
+        alert("Password did not match, please confirm your password!");
+    } else {
+        const uri = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/register";
+        let xhr = new XMLHttpRequest();
+
+        xhr.open("POST", uri, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        jreg = JSON.stringify(
+            {
+                Name: username,
+                Password: password
+            }
+        );
+
+        xhr.send(jreg);
+
+        xhr.onload = function () {
+            document.getElementById("username").value = "";
+            document.getElementById("reg-password").value = "";
+            document.getElementById("repeat-password").value = "";
+            document.querySelectorAll('.input.focus').forEach(function (element) {
+                element.className = element.className.replace(" focus", "");
+            })
+            alert(xhr.responseText);
+        }
+    }
+}
+
+function buyProduct(itemID) {
+    console.log(itemID);
+    window.open('http://redsox.uoa.auckland.ac.nz/dsa/Service.svc/buy?id=' + itemID);
+}
+let inputs = document.getElementsByClassName("input");
+
+for (i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("click", function () {
+        if (this.className.includes('focus')) {
+
+        } else {
+            this.className += " focus";
+        }
+    });
+}
+
+function showpass() {
+    let password = document.getElementById("password").value;
+    let passwd = document.getElementsByClassName("show");
+    if (password.length > 0) {
+        for (i = 0; i < passwd.length; i++) {
+            passwd[i].style.display = "flex";
+        }
+    } else {
+        for (i = 0; i < passwd.length; i++) {
+            passwd[i].style.display = "none";
+        }
+    }
+}
+
+const pass_field = document.querySelector('.password');
+const show_btn = document.querySelector('.show');
+
+show_btn.addEventListener('click', function () {
+    if (pass_field.type == "password") {
+        pass_field.type = "text";
+        show_btn.style.color = "#3498db";
+        show_btn.textContent = "HIDE";
+    } else {
+        pass_field.type = "password";
+        show_btn.style.color = "#222";
+        show_btn.textContent = "SHOW";
+    }
+});
+
+const login = document.querySelector('#login');
+const register = document.querySelector('#register');
+let regPasswd = document.getElementById('reg-passwd');
+let loginPasswd = document.getElementById('login-passwd');
+let repeat = document.getElementById('repeat');
+let loginButton = document.getElementById('login-btn');
+let regButton = document.getElementById('reg-btn');
+
+login.addEventListener('click', function () {
+    if (login.className.includes("active")) {
+
+    } else {
+        login.className += "active";
+        register.className = register.className.replace("active", "");
+        regPasswd.style.display = "none";
+        repeat.style.display = "none";
+        loginPasswd.style.display = "block";
+        regButton.style.display = "none";
+        loginButton.style.display = "block";
+        document.querySelectorAll('input').forEach(function (element) {
+            element.value = "";
+        })
+        document.querySelectorAll('.input.focus').forEach(function (element) {
+            element.className = element.className.replace(" focus", "");
+        })
+    }
+});
+
+register.addEventListener('click', function () {
+    if (register.className.includes("active")) {
+
+    } else {
+        register.className += "active";
+        login.className = login.className.replace("active", "");
+        regPasswd.style.display = "block";
+        repeat.style.display = "block";
+        loginPasswd.style.display = "none";
+        regButton.style.display = "block";
+        loginButton.style.display = "none";
+        document.querySelectorAll('input').forEach(function (element) {
+            element.value = "";
+        })
+        document.querySelectorAll('.input.focus').forEach(function (element) {
+            element.className = element.className.replace(" focus", "");
+        })
+    }
+});
 
 /** set time interval for key action to prevent fast typing issues*/
 var flag = 0;
